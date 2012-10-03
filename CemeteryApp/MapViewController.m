971ce933bce66aaa86d28556986da93d5741a271
Josh <jlisojo@kean.edu>
2012-10-03 14:19:29 -0400

    scrollable on the map

--- CemeteryApp/MapViewController.m
+++ CemeteryApp/MapViewController.m
@@ -8,12 +8,44 @@
 
 #import "MapViewController.h"
 
+@interface MapViewController() <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@end
+
 @implementation MapViewController
+@synthesize scrollView;
+@synthesize imageView;
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    self.scrollView.delegate = self;
+    self.scrollView.contentSize = self.imageView.image.size;
+    self.imageView.frame = CGRectMake(0, 0, self.imageView.image.size.width, self.imageView.image.size.height);
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
+    return self.imageView;
+}
+
+/*- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation{
+    if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone){
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else{
+        return YES;
+    }
+    
+}*/
+
+- (void)viewDidUnload {
+    [self setScrollView:nil];
+    [self setImageView:nil];
+    [super viewDidUnload];
+}
 
 - (IBAction) handlePinch:(UIPinchGestureRecognizer *)recognizer
 {
     recognizer.view.transform = CGAffineTransformScale(recognizer.view.transform, recognizer.scale, recognizer.scale);
     recognizer.scale = 1;
 }
-
 @end
