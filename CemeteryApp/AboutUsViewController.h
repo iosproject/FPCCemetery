7750dcb06089f9225a427a316d1e5a00421e69c9
Carlos Laptop <carlos.silva.6@gmail.com>
2013-02-13 15:04:53 -0500

    Uploaded the picture.

---
+++ CemeteryApp/AboutUsViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutUsViewController.h
+//  CemeteryApp
+//
+//  Created by Carlos Silva on 2/13/13.
+//  Copyright (c) 2013 KeanU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUsViewController : UIViewController {
+    IBOutlet UIScrollView *scroller;
+}
+
+@end
