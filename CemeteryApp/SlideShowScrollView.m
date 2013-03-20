d0de336dc3e9b1e7ddc50f732ceaab6ad594cfee
Computer Science1 <jlisojo@kean.edu>
2013-03-20 12:23:51 -0400

    slideshow class added

--- CemeteryApp/SlideShowScrollView.m
+++ CemeteryApp/SlideShowScrollView.m
@@ -1,50 +1,29 @@
 //
-//  CemeteryAppViewController.m
-//  CemeteryApp
+//  SlideShowScrollView.m
+//  BurialSearch
 //
-//  Created by student on 9/24/12.
-//  Copyright (c) 2012 KeanU. All rights reserved.
+//  Created by Joshua Lisojo on 3/20/13.
+//  Copyright (c) 2013 Lisojo. All rights reserved.
 //
 
-#import "HomeViewController.h"
-@interface HomeViewController ()
+#import "SlideShowScrollView.h"
 
-//@property (retain, nonatomic) IBOutlet UIScrollView *scrollView;
+@implementation SlideShowScrollView
 
-//@property (nonatomic, retain) NSArray *imgViews;
-//@property (nonatomic, retain) NSTimer *timer;
-
-@end
-
-
-@implementation HomeViewController
-@synthesize ssScrollView = _ssScrollView,
-            pageControl = _pageControl;
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    // setup the slideshow
-    //_ssScrollView = [[SlideShowScrollView alloc]initWithFrame:self.view.bounds];
-    [_ssScrollView setupSlideShow];
-    self.ssScrollView.pageControl = self.pageControl;
-    //[self.view addSubview:_ssScrollView];
-    
-}
-
-/*
-@synthesize scrollView = _scrollView,
+@synthesize imgViews = _imgViews,
 pageControl = _pageControl,
-imgViews = _imgViews,
 timer = _timer;
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    [self setupSlideShow];
-}
 
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self setupSlideShow];
+    }
+    return self;
+}
 
 // Setup the home tab's slideshow.
 - (void)setupSlideShow
@@ -64,12 +43,12 @@ timer = _timer;
     
     _imgViews = [NSArray arrayWithObjects: imageView1, imageView2, imageView3, imageView4, imageView5, imageView6, nil];
     
-    self.scrollView.pagingEnabled = YES;
-    self.scrollView.bounces = NO;
-    self.scrollView.showsHorizontalScrollIndicator = NO;
-    self.scrollView.showsVerticalScrollIndicator = NO;
-    self.scrollView.scrollsToTop = NO;
-    self.scrollView.delegate = self;
+    self.pagingEnabled = YES;
+    self.bounces = NO;
+    self.showsHorizontalScrollIndicator = NO;
+    self.showsVerticalScrollIndicator = NO;
+    self.scrollsToTop = NO;
+    self.delegate = self;
     
     CGRect cRect = imageView1.bounds;
     UIImageView *cView;
@@ -78,12 +57,12 @@ timer = _timer;
     {
         cView = [_imgViews objectAtIndex:i];
         cView.frame = cRect;
-        [self.scrollView addSubview:cView];
+        [self addSubview:cView];
         cRect.origin.x += cRect.size.width;
     }
     
-    self.scrollView.contentSize = CGSizeMake(cRect.origin.x, cRect.size.height);
-    [self.scrollView setContentOffset:CGPointMake(320, 0) animated:YES];
+    self.contentSize = CGSizeMake(cRect.origin.x, 0);
+    [self setContentOffset:CGPointMake(320, 0) animated:YES];
     [self startSlideShow];
     
 }
@@ -105,7 +84,7 @@ timer = _timer;
 // When the timer is done this method gets called.
 - (void) onTimer
 {
-    [self.scrollView setContentOffset:CGPointMake(self.scrollView.contentOffset.x + self.scrollView.frame.size.width, 0) animated:YES];
+    [self setContentOffset:CGPointMake(self.contentOffset.x + 320, 0) animated:YES];
 }
 
 // Reset the timer.
@@ -122,13 +101,13 @@ timer = _timer;
 // Things to do when the scroll view has scrolled.
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    if (self.scrollView.contentOffset.x == 0)
+    if (self.contentOffset.x == 0)
     {
-        [self.scrollView setContentOffset:CGPointMake(1280, 0) animated:NO];
+        [self setContentOffset:CGPointMake(1280, 0) animated:NO];
     }
-    else if (self.scrollView.contentOffset.x == 1600)
+    else if (self.contentOffset.x == 1600)
     {
-        [self.scrollView setContentOffset:CGPointMake(320, 0) animated:NO];
+        [self setContentOffset:CGPointMake(320, 0) animated:NO];
     }
     
     // Switch the indicator when more than 50% of the previous/next page is visible
@@ -141,5 +120,15 @@ timer = _timer;
 {
     [self resetTimer];
 }
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
 */
+
 @end
